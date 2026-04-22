5b97aba48b00be7ffaf774304dce6f482b50f8b4
kipavy <kiki.kalagan@gmail.com>
2026-04-23 00:05:31 +0200

    fix next config

--- app-portal/next.config.ts
+++ app-portal/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Required for wasm-bindgen output
+  turbopack: {},
   webpack(config) {
     config.experiments = { ...config.experiments, asyncWebAssembly: true };
     return config;
